9487ae01022b8189d18a30028feaf4db3d4e8e17
Marcel Hellkamp <marc@gsites.de>
2015-04-09 22:20:58 +0200

    Playing with an option parser

--- Makefile
+++ Makefile
@@ -1,14 +1,18 @@
-.PHONY: release debug clean
+.PHONY: init release debug clean
 
-release:
+release: init
 	-mkdir release
 	cd release; cmake -DCMAKE_BUILD_TYPE=Release ..
 	cd release; make
 
-debug:
+debug: init
 	-mkdir debug
 	cd debug; cmake -DCMAKE_BUILD_TYPE=Debug ..
 	cd debug; make
+
+init:
+	git submodule init
+	git submodule update
 
 clean:
 	-rm -rf debug release
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   main.cpp
  * Author: marc
  *
@@ -9,11 +9,38 @@
 #include <exception>
 #include <iostream>
 #include <thread>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstring>
 
 #include "PixelServer.h"
+namespace {
+    class OptParser {
+    public:
+        OptParser(int argc, char ** argv): argc(argc), argv(argv) {}
+        bool get(const char* name, int &a) {
+            for(int i=1; i<argc-1; i++) {
+                if(strcmp(name, argv[i]))
+                    continue;
+                a = std::stoi(argv[i+1]);
+                return true;
+            }
+            return false;
+        }
+    private:
+        int argc;
+        char ** argv;
+    };
+}
+
 
 int main(int argc, char* argv[]) {
-    
+    OptParser opt(argc, argv);
+
+    int port = 0;
+    opt.get("-p", port) || opt.get("--port", port);
+
     try {
         PixelServer px;
         px.run();
